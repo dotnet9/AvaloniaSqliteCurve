fe36db8a31f1b87dd2974dfdd827024c5ff15a1b
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-10-29 21:43:44 +0800

    update update interval;

--- src/AvaloniaSqliteCurve/Models/ConstData.cs
+++ src/AvaloniaSqliteCurve/Models/ConstData.cs
@@ -8,7 +8,7 @@ public class ConstData
     public const double MaxTop = 300.0;
 
     public const int AddDataInterval = 100;
-    public const int UpdateDataInterval = 1000;
+    public const int UpdateDataInterval = 10;
 
 
     public static Avalonia.Media.Color Fill = Avalonia.Media.Colors.White;

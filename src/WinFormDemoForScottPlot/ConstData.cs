b44d561ac10bbea8b0c5774c6a398084bf209c3d
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-10-30 10:31:16 +0800

    Upgrade NuGet;

--- src/WinFormDemoForScottPlot/ConstData.cs
+++ src/WinFormDemoForScottPlot/ConstData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WinFormDemoForScottPlot;
+﻿namespace WinFormDemoForScottPlot;
 
 public class ConstData
 {
--- src/WinFormDemoForScottPlot/Program.cs
+++ src/WinFormDemoForScottPlot/Program.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace WinFormDemoForScottPlot
 {
     internal static class Program
@@ -10,6 +12,7 @@ namespace WinFormDemoForScottPlot
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            ComWrappers.RegisterForMarshalling(WinFormsComInterop.WinFormsComWrappers.Instance);
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
